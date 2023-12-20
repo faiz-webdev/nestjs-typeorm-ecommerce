975ca1309ab0fd6e70b559afce629d7660b4e5e0
faizttn <faijuddin@tothenew.com>
2023-12-20 22:05:30 +0530

    create user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { Roles } from 'src/utility/common/user-roles.enum';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn({})
+  id: number;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ type: 'enum', enum: Roles, default: [Roles.USER] })
+  roles: Roles[];
+}
