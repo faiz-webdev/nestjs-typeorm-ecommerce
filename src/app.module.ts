932fe05f3b69852fa7ff31623391fe22434955c7
faizttn <faijuddin@tothenew.com>
2023-12-21 16:05:02 +0530

    create currentUserMiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/data-source';
 import { UsersModule } from './users/users.module';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './http-exception.filter';
+import { CurrentUserMiddleware } from './utility/middleware/current-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forRoot(dataSourceOptions), UsersModule],
@@ -15,4 +16,10 @@ import { HttpExceptionFilter } from './http-exception.filter';
     },
   ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CurrentUserMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
